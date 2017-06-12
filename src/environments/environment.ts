5065c325fe3c0e6f112ee0dc043ce94370142ab8
dressdonkey <el.estebes@gmail.com>
2017-06-12 21:11:35 +0100

    Other stuff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAqpk71ltRq2W2xM0SAhwVx_Sodxbt2W9U",
+    authDomain: "bbplanner-73bad.firebaseapp.com",
+    databaseURL: "https://bbplanner-73bad.firebaseio.com",
+    projectId: "bbplanner-73bad",
+    storageBucket: "bbplanner-73bad.appspot.com",
+    messagingSenderId: "932940984515"
+  }
 };
