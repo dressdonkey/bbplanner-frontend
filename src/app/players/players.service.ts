7ae835442ca0f5d878b06e8febe0b68d5a3b27e0
dressdonkey <el.estebes@gmail.com>
2018-03-08 19:33:05 +0000

    Revert "Revert "Revert "Merge branch 'master' into Practices"""
    This reverts commit 8ba6e861c965533948f28e8f0b73595a98d91fd4.

--- src/app/players/players.service.ts
+++ src/app/players/players.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Player } from './../interfaces/player';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { Player } from "./../interfaces/player";
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/Rx';
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
 @Injectable()
 export class PlayersService {
@@ -12,21 +12,21 @@ export class PlayersService {
   public deletedPlayer: Subject<any> = new Subject<any>();
   public updatedPlayer: Subject<any> = new Subject<any>();
 
-  constructor(private http: Http) {
-
+  constructor(private http: Http) { 
+    
   }
 
   /**
    * Get all the players
    */
 
-  getAllPlayers(): Observable<any> {
+  getAllPlayers(): Observable<any>{
     return this.http.get('http://192.168.33.10/api/players')
       .map(
-        (response: Response) => {
-
+        (response : Response) => {
+          
           return response.json().players;
-
+          
         }
       );
   }
@@ -36,39 +36,39 @@ export class PlayersService {
    * @param player player data json
    */
 
-  addPlayer(player: Player) {
-
+  addPlayer(player: Player){
+    
     const body = JSON.stringify(player);
     const hds = new Headers({'Content-Type': 'application/json'});
     return this.http.post('http://192.168.33.10/api/players', body, { headers: hds })
       .map(
         (response: Response) => {
-
+          
           this.addedPlayer.next(response.json());
-
+          
         }
       );
 
   }
 
   /**
-   *
+   * 
    * @param id player id
    * @param player player data json
    */
 
-  updatePlayer(id: number, player: Player) {
+  updatePlayer(id: number, player: Player){
 
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    const options = new RequestOptions({ headers: headers });
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
     const body = JSON.stringify(player);
-
-    return this.http.post('http://192.168.33.10/api/players/' + id, body, options)
+    
+    return this.http.post('http://192.168.33.10/api/players/'+id, body, options)
       .map(
         (response: Response) => {
-
+          
           this.updatedPlayer.next(response.json());
-
+          
         }
       );
   }
@@ -78,15 +78,15 @@ export class PlayersService {
    * @param id player id
    */
 
-  deletePlayer(id: number) {
-
+  deletePlayer(id: number){
+    
     return this.http.delete('http://192.168.33.10/api/players/' + id)
       .map(
         (response: Response) => {
           this.deletedPlayer.next(response.json());
         }
       );
-
+  
   }
 
 }
